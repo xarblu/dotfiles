daf0074d49c53cfa703c5198ef9c213fe36669d6
Xarblu <xarblu@protonmail.com>
2025-03-12 17:04:47 +0100

    nvim: don't map TAB for completions

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -119,26 +119,6 @@ return {
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif snippy.can_expand_or_advance() then
-                            snippy.expand_or_advance()
-                        elseif has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif snippy.can_jump(-1) then
-                            snippy.previous()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
                     ['<CR>'] = cmp.mapping(function(fallback)
                         if cmp.visible() and cmp.get_active_entry() then
                             cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
