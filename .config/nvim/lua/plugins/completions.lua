5e8b74336d84ccbf3ee9a1ae0d80c8c0be910be7
Xarblu <xarblu@protonmail.com>
2025-07-25 19:04:46 +0200

    nvim: move to blink.cmp and cleanup lsp config

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,182 +1,100 @@
 return {
-    -- LSP Client
+    -- LSP Servers and Diagnostics
     {
-        "neovim/nvim-lspconfig",
-        version = "*",
+        'neovim/nvim-lspconfig',
+        version = '*',
         config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            -- structure:
-            -- lspconfig.<server>.setup({})
-            -- see help lspconfig-all
-            -- or https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-            -- dev-lang/rust[rust-analyzer]::gentoo
-            lspconfig.rust_analyzer.setup({
-                capabilities = capabilities
-            })
-            -- sys-devel/clang::gentoo
-            lspconfig.clangd.setup({
-                capabilities = capabilities
-            })
-            -- dev-util/bash-language-server::guru
-            lspconfig.bashls.setup({
-                capabilities = capabilities
-            })
-            -- dev-util/yaml-language-server::guru
-            lspconfig.yamlls.setup({
-                capabilities = capabilities
-            })
-            -- dev-util/lua-language-server::guru
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = {"vim"} -- ignore for e.g. init.lua
-                        }
-                    }
-                }
-            })
-            -- pyright (pipx)
-            lspconfig.pyright.setup({
-                capabilities = capabilities
-            })
-            -- cmake-language-server (pipx)
-            lspconfig.cmake.setup({
-                capabilities = capabilities
-            })
-            -- nginx-language-server (pipx)
-            lspconfig.nginx_language_server.setup({
-                capabilities = capabilities
-            })
+            -- available configs: https://github.com/neovim/nvim-lspconfig/tree/master/lsp
+            -- vim.lsp.config('<ls>, {<config>}') -- for extra config
+            -- vim.lsp.enable('<ls>') -- to enable
 
-            -- Global mappings.
-            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+            vim.lsp.enable('ansiblels')
+            vim.lsp.enable('bashls') -- dev-util/bash-language-server
+            vim.lsp.enable('clangd') -- llvm-core/clang
+            vim.lsp.enable('cmake')
+            vim.lsp.enable('lua_ls') -- dev-util/lua-language-server
+            vim.lsp.enable('pylsp') -- dev-python/python-lsp-server (+ dev-python/python-lsp-mypy)
+            vim.lsp.enable('rust_analyzer') -- dev-lang/rust[rust-analyzer]
+            vim.lsp.enable('yamlls') -- dev-util/yaml-language-server
 
-            -- Use LspAttach autocommand to only map the following keys
-            -- after the language server attaches to the current buffer
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-                callback = function(ev)
-                    -- Enable completion triggered by <c-x><c-o>
-                    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-                    -- Buffer local mappings.
-                    -- See `:help vim.lsp.*` for documentation on any of the below functions
-                    local opts = { buffer = ev.buf }
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-                    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                    vim.keymap.set('n', '<space>wl', function()
-                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                    end, opts)
-                    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                    vim.keymap.set('n', '<space>f', function()
-                        vim.lsp.buf.format { async = true }
-                    end, opts)
-                end
+            -- how diagnostics should be shown by default
+            -- https://neovim.io/doc/user/diagnostic.html#vim.diagnostic.Opts
+            vim.diagnostic.config({
+                -- underline errors
+                underline = true,
+                -- show errors at EOL
+                virtual_text = false,
+                -- show errors on "virtual lines"
+                virtual_lines = true,
+                -- show diagnostic signs
+                signs = true,
+                -- whether to update in insert mode
+                -- (if false only on insert->normal transition)
+                update_in_insert = true,
+                -- sort errors before warnings
+                severity_sort = true,
             })
+            -- some styling
+            -- guisp requires usstyle in TMUX terminal-features
+            vim.cmd('highlight DiagnosticUnderlineError gui=undercurl guisp=LightRed')
+            vim.cmd('highlight DiagnosticUnderlineWarn gui=undercurl guisp=LightYellow')
+            vim.cmd('highlight DiagnosticUnderlineInfo gui=undercurl guisp=LightCyan')
+            vim.cmd('highlight DiagnosticUnderlineHint gui=undercurl guisp=LightGray')
         end
     },
 
     -- Completions
     {
-        "hrsh7th/nvim-cmp",
-        -- load cmp on InsertEnter
-        event = "InsertEnter",
-        config = function()
-            local has_words_before = function()
-                unpack = unpack or table.unpack
-                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil end
-            local cmp = require("cmp")
-            local snippy = require("snippy")
-            cmp.setup({
-                snippet = {
-                    -- REQUIRED - you must specify a snippet engine
-                    expand = function(args)
-                        require('snippy').expand_snippet(args.body)
-                        --vim.snippet.expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-e>'] = cmp.mapping.abort(),
-                    ['<CR>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() and cmp.get_active_entry() then
-                            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-                        else
-                            fallback()
-                        end
-                    end, {"i", "s", "c"}),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'snippy' },
-                    { name = 'path' },
-                    { name = 'buffer' }
-                })
-            })
+        'saghen/blink.cmp',
+        -- optional: provides snippets for the snippet source
+        dependencies = { 'rafamadriz/friendly-snippets' },
 
-            -- Set configuration for specific filetype.
-            cmp.setup.filetype('gitcommit', {
-                sources = cmp.config.sources({
-                    { name = 'git' },
-                    { name = 'buffer' }
-                })
-            })
+        -- use a release tag to download pre-built binaries
+        version = '*',
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
 
-            -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = 'buffer' }
-                }
-            })
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+            -- 'super-tab' for mappings similar to vscode (tab to accept)
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-n/C-p or Up/Down: Select next/previous item
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help (if signature.enabled = true)
+            --
+            -- See :h blink-cmp-config-keymap for defining your own keymap
+            keymap = { preset = 'enter' },
 
-            -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' },
-                    { name = 'cmdline' }
-                })
-            })
-        end,
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "petertriho/cmp-git",
-            {
-                "dcampos/cmp-snippy",
-                dependencies = {
-                    {
-                        "dcampos/nvim-snippy",
-                        dependencies = {
-                            "honza/vim-snippets"
-                        }
-                    }
-                }
-            }
+            appearance = {
+                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono'
+            },
+
+            -- (Default) Only show the documentation popup when manually triggered
+            completion = { documentation = { auto_show = false } },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+
+            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" }
+        },
+            opts_extend = { "sources.default" }
         }
-    }
 }
--- .config/nvim/lua/plugins/syntax.lua
+++ .config/nvim/lua/plugins/syntax.lua
@@ -1,25 +1,23 @@
 return {
     -- Treesitter for almost any syntax
     {
-        "nvim-treesitter/nvim-treesitter",
-        version = "*",
-        build = ":TSUpdate"
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        branch = 'main',
+        build = ':TSUpdate'
     },
     
     -- Syntax for Gentoo things like ebuilds
     {
-        "gentoo/gentoo-syntax",
-        version = "*",
-        dependencies = {
-            { "dense-analysis/ale" }
-        }
+        'gentoo/gentoo-syntax',
+        version = '*'
     },
 
     -- Indent marker lines
     {
-        "lukas-reineke/indent-blankline.nvim",
-        version = "*",
-        main = "ibl",
+        'lukas-reineke/indent-blankline.nvim',
+        version = '*',
+        main = 'ibl',
         opts = {}
     },
 
