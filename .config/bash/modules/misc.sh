69f506b022a077ec69758b40284ca1f73cc2fdf7
Xarblu <xarblu@protonmail.com>
2025-07-31 09:45:52 +0200

    bash: set histappend

--- .config/bash/modules/misc.sh
+++ .config/bash/modules/misc.sh
@@ -4,4 +4,7 @@
 function __misc_setup__() {
     mkdir_if_not_exists "${BASH_DATADIR}"
     mkfile_if_not_exists "${HISTFILE}"
+
+    # append HISTFILE on shell exit instead of overwriting
+    shopt -s histappend
 }
