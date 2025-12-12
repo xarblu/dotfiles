a2090d464945ee0f34e45c44259cb507f26d0fe9
Xarblu <xarblu@protonmail.com>
2025-12-12 14:59:25 +0100

    bash: remove gz wrapper

--- .config/bash/modules/containers.sh
+++ .config/bash/modules/containers.sh
@@ -14,18 +14,4 @@ function __containers_init_jazzy__() {
     else
         log --error "Setup script does not exist at %s" "${setup}"
     fi
-
-    local gz_wrapper="/usr/local/bin/gz"
-    if [[ ! -x "${gz_wrapper}" ]]; then
-        log --info "Installing gz (gazebo) wrapper at %s" "${gz_wrapper}"
-        "${SUDO:-sudo}" tee "${gz_wrapper}" <<EOF
-#!/usr/bin/env bash
-export QT_QPA_PLATFORM=wayland
-export PATH="\${PATH//\\/usr\\/local\\/bin:/}"
-exec gz "\${@}"
-EOF
-        "${SUDO:-sudo}" chmod +x "${gz_wrapper}"
-    fi
-
-    export PATH="/usr/local/bin${PATH:+:}${PATH}"
 }
